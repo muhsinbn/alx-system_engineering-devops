377c24cab848ec70c4ff7960db4686c44645a7c9
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-03-12 06:31:01 -0700

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,17 @@
 #!/usr/bin/env ruby
 
 # Check if the argument matches the regular expression
-
 def match_school(argument)
-  regex = /School/
-  if argument =~ regex
+  if argument.match?(/School/)
     puts "The argument '#{argument}' matches 'School'."
   else
     puts "The argument '#{argument}' does not match 'School'."
   end
 end
 
-#main script
-
+# Main script
 if ARG.length == 1
   match_school(ARG[0])
 else
-  puts "Usage: ruby scripts_name.rb <argument>"
+  puts "Usage: ruby script_name.rb <argument>"
 end
