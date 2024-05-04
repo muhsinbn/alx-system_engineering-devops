9983c31593bc578c3b600837fdce05b1f6bcd4d0
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-05-04 08:49:22 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Create file in /tmp
 
 file { '/tmp/school' :
-    path    => 'file',
+    path    => '/tmp/school',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
     }
+
