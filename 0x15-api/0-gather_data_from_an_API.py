b082920f6d283d5efc88a47f391ade36e453c760
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-06-03 09:45:52 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-# This script retrieves and displays the TODO list progress of a given employee based on the employee ID
-
+"""
+This script fetches and displays the TODO list progress of an
+employee from a REST API.
+"""
 import requests
+import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
 
-    employee_response = requests.get(employee_url)
-    todos_response = requests.get(todos_url)
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    if employee_response.status_code != 200 or todos_response.status_code != 200:
-        print("Failed to retrieve data. Please try again.")
-        return
-
-    employee_data = employee_response.json()
-    todos_data = todos_response.json()
-
-    employee_name = employee_data.get("name")
-    total_tasks = len(todos_data)
-    done_tasks = [task["title"] for task in todos_data if task["completed"]]
-
-    print(f"Employee {employee_name} is done with tasks ({len(done_tasks)}/{total_tasks}):")
-    print(f"{employee_name}: {len(done_tasks)} completed tasks out of {total_tasks} total tasks")
-
-    for task in done_tasks:
-        print(f"\t{task}")
-
-employee_id = int(input("Enter the employee ID: "))
-get_employee_todo_progress(employee_id)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
